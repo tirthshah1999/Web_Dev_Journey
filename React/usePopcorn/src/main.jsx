b716bc02145f764fe9119b964ee25d6a145cac51
tirth <tirthcoder@gmail.com>
2026-09-05 16:44:50 +0530

    working on star rating component

---
+++ React/usePopcorn/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
+import StarRating from "./StarRating.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+  </StrictMode>,
+);
